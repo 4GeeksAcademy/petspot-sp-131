b5b861f4305af78fb7aec602541947d5c401011b
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-04-21 12:56:25 +0200

    Add GET /favorites endpoint

--- src/api/models.py
+++ src/api/models.py
@@ -15,7 +15,10 @@ class User(db.Model):
     is_active: Mapped[bool] = mapped_column(Boolean(), default=True)
 
     # Relationship Many - Many
-    favorite_places: Mapped[list["Favorite"]] = relationship("Favorite", back_populates="user")
+    favorite_places: Mapped[list["Favorite"]] = relationship("Favorite", back_populates="user", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return self.name
 
     def serialize(self):
         return {
@@ -48,7 +51,7 @@ class Place(db.Model):
     # Relationship Many - One
     city: Mapped["City"] = relationship("City", back_populates="places")
     # Relationship Many - Many
-    favorites: Mapped[list["Favorite"]] = relationship("Favorite", back_populates = "place")
+    favorites: Mapped[list["Favorite"]] = relationship("Favorite", back_populates = "place", cascade="all, delete-orphan")
 
     def __str__(self):
         return self.name
@@ -121,7 +124,9 @@ class Favorite(db.Model):
         return {
             "id": self.id,
             "user_id": self.user_id,
-            "place_id": self.place_id
+            "user_name": self.user.name,
+            "place_id": self.place_id,
+            "place_name": self.place.name
         }
 
 
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Place, EstablishmentType, AdminUser, City
+from api.models import db, User, Place, EstablishmentType, AdminUser, City, Favorite
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from sqlalchemy import select
@@ -412,4 +412,9 @@ def update_city(city_id):
         return jsonify(response="City cannot be updated to an existing city name"), 400
     
     return jsonify(city_exists.serialize()), 200
+
+@api.route('favorites', methods=['GET'])
+def get_favorites():
+    favorites = db.session.execute(select(Favorite)).scalars().all()
+    return jsonify([favorite.serialize() for favorite in favorites]), 200
 
