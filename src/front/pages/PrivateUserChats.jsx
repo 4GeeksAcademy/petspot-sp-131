96b20dd13b308ce2315e9ce0c36a43097a47f9a7
AdminSantrax <jairomt95@gmail.com>
2026-04-27 15:50:44 +0200

    Fix import path in PrivateUserChats.jsx

--- src/front/pages/PrivateUserChats.jsx
+++ src/front/pages/PrivateUserChats.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import MessageInbox from "../../components/Chat/MessageInbox";
+import MessageInbox from "../components/Chat/MessageInbox";
 
 const PrivateUserChats = () => {
     return (
