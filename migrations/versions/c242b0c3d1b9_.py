99be7ed947b1d418c874d08b0eda929f64f7cab7
AdminSantrax <jairomt95@gmail.com>
2026-04-29 01:12:52 +0200

    restore: bring back missing migrations and install leaflet dependencies

---
+++ migrations/versions/c242b0c3d1b9_.py
@@ -0,0 +1,21 @@
+"""dummy message
+
+Revision ID: c242b0c3d1b9
+Revises: 441726df2a1a
+Create Date: 2026-04-27 12:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'c242b0c3d1b9'
+down_revision = '441726df2a1a'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
