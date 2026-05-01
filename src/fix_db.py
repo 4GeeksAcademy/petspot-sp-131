b4631af39671a94c89afffcebfe07b6898861109
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-05-01 19:18:59 +0200

    remove scratch files and fix_db script

--- src/fix_db.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-with app.app_context():
-    db.session.execute(db.text("UPDATE places SET establishment_type='RESTAURANT' WHERE establishment_type='restaurant'"))
-    db.session.execute(db.text("UPDATE places SET establishment_type='BAR' WHERE establishment_type='bar'"))
-    db.session.execute(db.text("UPDATE places SET establishment_type='CAFE' WHERE establishment_type='cafe'"))
-    db.session.commit()
-    print("Fixed!")
