02be8b5ce6dcbea2c84fa8b9202971bc0f703aba
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-05-06 16:13:17 +0200

    adjust navbar

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -4,11 +4,18 @@ import HowItWorksSection from "../components/Home/HowItWorksSection";
 import BusinessesSection from "../components/Home/BusinessesSection";
 import ReviewsSection from "../components/Home/ReviewsSection";
 import AboutSection from "../components/Home/AboutSection";
+import { Navbar } from "../components/Navbar";
 
 function Home() {
 
     return (
         <>
+            <header>
+
+                <Navbar />
+
+            </header>
+            
             <main className="home">
 
                 <HeroSection />
